0ceb5b89488a14b4006aa07b60ec84cec458f50d
savagecapricon-alt <savagecapricon@gmail.com>
2025-11-20 10:40:52 +0100

    Add files via upload
    This assignment tests the skills and level of understanding of the creation, modification and use of lists and index in pyt

---
+++ Assignments wt py page 42 and 46.py
@@ -0,0 +1,85 @@
+1#3-4 guest list
+guest_list=['Sue', 'Prince', 'Melvis', 'Laurencia', 'Ellen', 'Kelly' ]
+print(guest_list[0], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[1], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[2], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[3], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[4], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[5], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+
+#3-5 changing the guset list
+guest_list=['Sue', 'Prince', 'melvis', 'Laurencia', 'Ellen', 'Kelly' ]
+print(guest_list[0], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[1], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[2], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[3], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[4], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+print(guest_list[5], 'you are cordially invited to a casual diner on the 01/12/25 please RSVP')
+
+print(guest_list[3], 'Dear host, unfortunately i must RSVP NO to you dinner invitations')
+
+guest_list.pop(3)
+print(guest_list)
+guest_list.insert(3, 'Brian')
+print(guest_list)
+
+print(guest_list[0], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[1], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[2], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[3], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[4], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[5], 'you are ivited to a casual dinner on the 01/12/25')
+
+#3-6 More guests
+print(guest_list)
+
+print(guest_list)
+guest_list.insert(3, 'Brian')
+print(guest_list)
+
+print(guest_list[0], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[1], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[2], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[3], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[4], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[5], 'you are ivited to a casual dinner on the 01/12/25')
+
+print('Dear guests, i humbly inform you that i your host has found a bigger table and so our casual dinner has been expanded inorder to accomodate more people.Thank you for understanding')
+
+guest_list.insert(0, 'Bemsi')
+guest_list.insert(4, 'Ethel')
+guest_list.append('martina')
+print(guest_list)
+
+print(guest_list[0], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[1], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[2], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[3], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[4], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[5], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[6], 'you are ivited to a casual dinner on the 01/12/25')
+print(guest_list[7], 'you are ivited to a casual dinner on the 01/12/25')
+
+#3-8seeing the world
+travel_list=['Thailand', 'Swedish alps', 'Bali', 'Geat wall of china', 'japan']
+print(travel_list)
+
+travel_list.sort()
+print(travel_list)
+
+travel_list.sort(reverse=True)
+print(travel_list)
+
+travel_list.reverse()
+print(travel_list)
+
+travel_list.reverse()
+print(travel_list)
+
+travel_list.sort()
+print(travel_list)
+
+travel_list.sort(reverse=True)
+print(travel_list)
+
+
